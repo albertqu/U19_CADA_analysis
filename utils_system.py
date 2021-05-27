90f3163f5a40193fc84c1bc70c925aee1e471168
albertqu <albert_qu@berkeley.edu>
2021-05-27 15:46:04 -0700

    system changes

--- utils_system.py
+++ utils_system.py
@@ -1,7 +1,26 @@
 import os, shutil, stat, errno, time, datetime
 
-def filter_by_date(src_folder, dest_folder, archive_date):
+
+def archive_by_date(src_folder, dest_folder, archive_date):
     os.chdir(src_folder)
+    tomoves = []
     for name in os.listdir(src_folder):
-        if datetime.datetime.fromtimestamp(os.path.getmtime(name)) < archive_date:
-            print(name, datetime.datetime.fromtimestamp(os.path.getmtime(name)))
+        if (name != 'ARCHIVE') and (datetime.datetime.fromtimestamp(os.path.getmtime(name)) < archive_date):
+            # print(name, datetime.datetime.fromtimestamp(os.path.getmtime(name)))
+            tomoves.append(name)
+    for name in tomoves:
+        try:
+            print("moving", name)
+            shutil.move(name, dest_folder)
+        except Exception:
+            print("skipping", name)
+
+
+if __name__ == '__main__':
+    archive_date = datetime.datetime.strptime("2019/05/31", "%Y/%m/%d")
+    src_folder = "/Volumes/Wilbrecht_file_server"
+    archive = os.path.join(src_folder, 'ARCHIVE')
+    if not os.path.exists(archive):
+        os.makedirs(archive)
+    archive_by_date(src_folder, archive, archive_date)
+
