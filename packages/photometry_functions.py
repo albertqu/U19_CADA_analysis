8731719dacfdc6d26e421ab08f3eeab8f6856e62
Albert Qu <albert_qu@berkeley.edu>
2022-01-10 20:22:14 -0800

    updated fp quality visualization and some minor changes in PS_expr

--- packages/photometry_functions.py
+++ packages/photometry_functions.py
@@ -260,49 +260,6 @@ def get_zdFF_old(reference,signal,smooth_win=10,remove=200,lambd=5e4,porder=1,it
         return zdFF
 
 
-def jove_fit_reference(reference, signal, smooth_win=10, remove=200, lambd=5e4, porder=1, itermax=50):
-    '''
-    Calculates z-score dF/F signal based on fiber photometry calcium-idependent
-    and calcium-dependent signals
-
-    Input
-        reference: calcium-independent signal (usually 405-420 nm excitation), 1D array
-        signal: calcium-dependent signal (usually 465-490 nm excitation for
-                     green fluorescent proteins, or ~560 nm for red), 1D array
-        smooth_win: window for moving average smooth, integer
-        remove: the beginning of the traces with a big slope one would like to remove, integer
-        Inputs for airPLS:
-        lambd: parameter that can be adjusted by user. The larger lambda is,
-                the smoother the resulting background, z
-        porder: adaptive iteratively reweighted penalized least squares for baseline fitting
-        itermax: maximum iteration times
-    Output
-        zdFF - z-score dF/F, 1D numpy array
-    '''
-
-    # Smooth signal
-    reference = smooth_signal(reference, smooth_win)
-    signal = smooth_signal(signal, smooth_win)
-
-    # Remove slope using airPLS algorithm
-    r_base = airPLS(reference, lambda_=lambd, porder=porder, itermax=itermax)
-    s_base = airPLS(signal, lambda_=lambd, porder=porder, itermax=itermax)
-
-    # Remove baseline and the begining of recording
-    reference = (reference[remove:] - r_base[remove:]) + np.mean(signal)
-    signal = (signal[remove:] - s_base[remove:]) + np.mean(signal)
-
-    # Standardize signals
-    # reference = (reference - np.median(reference)) / np.std(reference)
-    # signal = (signal - np.median(signal)) / np.std(signal)
-
-    # Align reference signal to calcium signal using non-negative robust linear regression
-    lin = Lasso(alpha=0.0001, precompute=True, max_iter=1000,
-                positive=True, random_state=9999, selection='random')
-    n = len(reference)
-    lin.fit(reference.reshape(n, 1), signal.reshape(n, 1))
-    return lin.predict(reference.reshape(n, 1)).reshape(n, )
-
 def get_f0_Martianova_jove(reference,signal,smooth_win=40,remove=0,lambd=5e4,porder=1,itermax=50):
     '''
     Calculates z-score dF/F signal based on fiber photometry calcium-idependent
